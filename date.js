22ead9a2e51363c36f0b3c98bd1e3fb9577112bc
Larysa Haurylavets <Larysa_Haurylavets@epam.com>
2017-09-14 11:39:43 +0300

    Edit file date.js

--- date.js
+++ date.js
@@ -1,2 +1,3 @@
 var date = new Date();
-console.log(date.getFullYear() + '-' + date.getMonth() + '-' + date.getDate());
+console.log(date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds() + ' ' + date.getFullYear() + '-' + date.getMonth() + '-' + date.getDate());
+console.log(date.getTime());
